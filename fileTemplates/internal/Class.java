f3b8682ea0e2197d7898342c34ab9ac34be911d9
hcs <xs199510>
2023-04-19 16:00:31 +0800

    IU-2019.3.1 <xxs@DESKTOP-E7A1QVD Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, web-types-registry.xml, CodeGlance.xml, project.default.xml, diff.xml, lombok-plugin.xml, Material Oceanic copy.icls, yiiguxing.translation.xml, terminal.xml, Default.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, keymap.xml, androidEditors.xml, colors.scheme.xml, _@user_Darcula.icls, databaseDrivers.xml, my.xml, mavenVersion.xml, templates.xml, GrepConsole.xml, laf.xml, Class.java, jdk.table.xml, path.macros.xml, Eclipse copy.xml, Ladies Night 2.xml, wsl.distributions.xml, material_theme.xml, vcs.xml, codota.xml, mybatis.xml, editor.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * 
+ * @Author xxs
+ * @Date ${DATE} ${TIME}
+ * @Version 1.0
+ */
+public class ${NAME} {
+}
